59017bbc86c0ac5c7267d27809f45a38325b2afe
Shivasai011 <shivasaibogem@gmail.com>
2024-09-04 21:09:49 +0530

    file

--- slave.sh
+++ slave.sh
@@ -1,12 +1,14 @@
+sudo apt install openjdk-17-jre
 sudo apt-get update
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins -y
-sudo systemctl enable jenkins 
-sudo systemctl start jenkins
+sudo apt-get install jenkins
+sudo systemctl enable jenkins  
+sudo systemctl start jenkins  
 sudo apt install docker.io -y
+sudo usermod -aG docker $USER
 sudo chmod 777 /var/run/docker.sock
